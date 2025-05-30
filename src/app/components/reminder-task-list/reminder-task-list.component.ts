9fb56b387a96d548e6b15c396f57d4f108afb974
thedakotes <lantripdakota@gmail.com>
2025-05-30 11:00:46 -0400

    Remove unused code

--- src/app/components/reminder-task-list/reminder-task-list.component.ts
+++ src/app/components/reminder-task-list/reminder-task-list.component.ts
@@ -1,26 +1,17 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { DatePipe } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
 
 import { MatSymbolDirective } from '../../directives/mat-symbol.directive';
 import { PriorityLevelEnum, ReminderTask } from '../../models/reminder.model';
-import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-reminder-task-list',
-  imports: [
-    DatePipe,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    MatMenuModule,
-    MatSymbolDirective,
-    MatTooltipModule,
-  ],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatMenuModule, MatSymbolDirective, MatTooltipModule],
   templateUrl: './reminder-task-list.component.html',
   styleUrl: './reminder-task-list.component.scss',
 })
@@ -33,10 +24,6 @@ export class ReminderTaskListComponent {
   ) {}
 
   //#region Events
-
-  public onAddClick(): void {
-    this.bottomSheetRef.dismiss({ action: 'add' });
-  }
 
   public onCompleteClick(task: ReminderTask): void {
     this.bottomSheetRef.dismiss({ id: task.id, action: 'complete' });
