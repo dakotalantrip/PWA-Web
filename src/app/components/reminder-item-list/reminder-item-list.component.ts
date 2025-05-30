23ef05fc02ba9ae36ca7537b18055ad3497beaad
thedakotes <lantripdakota@gmail.com>
2025-05-30 11:00:10 -0400

    Simplify list UI and move add to menu instead of taking up space on bottom sheet

--- src/app/components/reminder-item-list/reminder-item-list.component.ts
+++ src/app/components/reminder-item-list/reminder-item-list.component.ts
@@ -1,26 +1,17 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { DatePipe } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
 
 import { MatSymbolDirective } from '../../directives/mat-symbol.directive';
 import { ReminderItem } from '../../models/reminder.model';
-import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-reminder-item-list',
-  imports: [
-    DatePipe,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    MatMenuModule,
-    MatSymbolDirective,
-    MatTooltipModule,
-  ],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatMenuModule, MatSymbolDirective, MatTooltipModule],
   templateUrl: './reminder-item-list.component.html',
   styleUrl: './reminder-item-list.component.scss',
 })
@@ -31,10 +22,6 @@ export class ReminderItemListComponent {
   ) {}
 
   //#region Events
-
-  public onAddClick(): void {
-    this.bottomSheetRef.dismiss({ action: 'add' });
-  }
 
   public onDeleteClick(item: ReminderItem): void {
     this.bottomSheetRef.dismiss({ id: item.id, action: 'delete' });
